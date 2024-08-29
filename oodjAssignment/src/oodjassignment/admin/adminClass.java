82093e125f519d3905f00e5c5ba820ec70f09996
Desmond23Y <tp066933@mail.apu.edu.my>
2024-08-29 15:51:59 +0800

    Desmond made changes

--- oodjAssignment/src/oodjassignment/admin/adminClass.java
+++ oodjAssignment/src/oodjassignment/admin/adminClass.java
@@ -37,7 +37,7 @@ public class adminClass {
 
     public static void showDataFromFile(String filePath, DefaultTableModel upcomingModel, DefaultTableModel pastModel) {
         // Date format: 01-Jul-2024
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy", Locale.ENGLISH);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
         // Time format: 5pm
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("ha");
 
@@ -48,7 +48,6 @@ public class adminClass {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
-                // Assuming the file format is: BookingID, Customer Name, Hall type, Date, Time, Duration, Price, Event Information
                 String bookingDate = data[3];
                 String bookingTime = data[4];
 
