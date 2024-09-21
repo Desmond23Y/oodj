6a46fc77420970536fc72520809e8d3bd26f660a
User <User@10.101.51.59>
2024-09-21 23:28:23 +0800

    Merge origin/master
    Conflicts:
    	oodjAssignment/src/oodjassignment/database/Booking.txt
    	oodjAssignment/src/oodjassignment/database/Schedule.txt

---
+++ oodjAssignment/src/oodjassignment/user/GenerateReceipt.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodjassignment.user;
+
+/**
+ *
+ * @author User
+ */
+public class GenerateReceipt {
+    
+}
